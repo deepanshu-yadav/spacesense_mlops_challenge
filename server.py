cb161ff8a22813d104f27e48e467d868e165f86d
Deepanshu Yadav <rohantheyadav@gmail.com>
2024-05-08 23:11:44 +0200

    chnaged files to make fast api work
    Signed-off-by: Deepanshu Yadav <rohantheyadav@gmail.com>

---
+++ server.py
@@ -0,0 +1,78 @@
+import os
+import io
+
+from fastapi import FastAPI, File
+from starlette.responses import Response
+
+import numpy as np
+import torch
+from mobile_sam import SamAutomaticMaskGenerator, SamPredictor, sam_model_registry
+from PIL import Image
+
+from tools import box_prompt, format_results, point_prompt, fast_process
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+sam_checkpoint = "./mobile_sam.pt"
+model_type = "vit_t"
+
+mobile_sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+mobile_sam = mobile_sam.to(device=device)
+mobile_sam.eval()
+
+mask_generator = SamAutomaticMaskGenerator(mobile_sam)
+predictor = SamPredictor(mobile_sam)
+
+@torch.no_grad()
+def segment_everything(
+    image,
+    input_size=1024,
+    better_quality=False,
+    withContours=True,
+    use_retina=True,
+    mask_random_color=True,
+):
+    global mask_generator
+
+    input_size = int(input_size)
+    w, h = image.size
+    scale = input_size / max(w, h)
+    new_w = int(w * scale)
+    new_h = int(h * scale)
+    image = image.resize((new_w, new_h))
+
+    nd_image = np.array(image)
+    annotations = mask_generator.generate(nd_image)
+
+    fig = fast_process(
+        annotations=annotations,
+        image=image,
+        device=device,
+        scale=(1024 // input_size),
+        better_quality=better_quality,
+        mask_random_color=mask_random_color,
+        bbox=None,
+        use_retina=use_retina,
+        withContours=withContours,
+    )
+    return fig
+
+
+app = FastAPI(
+    title="DeepLabV3 image segmentation",
+    description="""Obtain semantic segmentation maps of the image in input via DeepLabV3 implemented in PyTorch.
+                           Visit this URL at port 8501 for the streamlit interface.""",
+    version="0.1.0",
+)
+
+print("done..")
+
+@app.post("/segmentation")
+def get_segmentation_map(file: bytes = File(...)):
+    """Get segmentation maps from image file"""
+    print("yes")
+    input_image = Image.open(io.BytesIO(file)).convert("RGB")
+    segmented_image = segment_everything(input_image)
+    bytes_io = io.BytesIO()
+    segmented_image.save(bytes_io, format="PNG")
+    return Response(bytes_io.getvalue(), media_type="image/png")
